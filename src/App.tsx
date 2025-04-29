8edea2a9f5e87bcdfa12d40cf13136dde2cca490
amandabrig <amandagsampaio@hotmail.com>
2025-04-29 14:37:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Progresso from './pages/Progresso';
 import Desafios from './pages/Desafios';
+import Registro from './pages/Registro'; // ⬅️ esse import tem que ficar aqui em cima também
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/progresso" element={<Progresso />} />
         <Route path="/desafios" element={<Desafios />} />
+        <Route path="/registro" element={<Registro />} /> {/* ⬅️ adicionando a rota */}
       </Routes>
     </Router>
   );
